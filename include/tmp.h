a36659f0ddd1bcddaf8230e42eb6f018d830ea88
lq-debuger <lfszliqiang@163.com>
2022-05-31 23:48:17 +0800

    add implicit demo code

--- include/tmp.h
+++
@@ -1,7 +0,0 @@
-/*
- * @Description:
- * @version:
- * @Author: liqiang
- * @Date: 2022-04-30 22:01:02
- * @LastEditTime: 2022-04-30 22:01:03
- */
--- src/explicit.cc
+++ src/explicit.cc
@@ -1,18 +1,29 @@
-static char help[] = "Solves a tridiagonal linear system.\n\n";
+static char help[] = "Solves the transient heat equation with implicit scheme.\n\n";
 
 #include <stdio.h>
 #include <petscksp.h>
+#include <math.h>
 
 int main(int argc,char **args)
 {
-  Vec            y, z;          
+  Vec            u_old, u;          
   Mat            A;
   KSP            ksp;
   PC             pc;                  
-  PetscReal      norm,norm_old=1.0,tol=1000.*PETSC_MACHINE_EPSILON;  
+  PetscReal      norm,norm_old=1.0,kappa,dt,dx,rho,c,alpha,beta,tol=1000.*PETSC_MACHINE_EPSILON;  
   PetscErrorCode ierr;
-  PetscInt       i,n = 10,col[3],rstart,rend,nlocal,maxit=10000;
-  PetscScalar    value[3],value_vec,lambda;
+  PetscInt       i,n = 256,col[3],rstart,rend,rank,nlocal,steps,maxit=10000;
+  PetscScalar    value[3],value_vec,lambda,xi;
+
+  kappa= 1.0;
+  dt   = 0.0002;
+  steps= 20.0/dt;
+  dx   = 1.0/n;
+  rho  = 1.0;
+  c    = 1.0;
+  alpha= -(kappa*dt/rho/c/dx/dx);
+  beta = 1-2*alpha;
+  i    = 0;
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
@@ -20,54 +31,66 @@ int main(int argc,char **args)
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
 
-  ierr = VecCreate(PETSC_COMM_WORLD,&z);CHKERRQ(ierr);
-  ierr = VecSetSizes(z,PETSC_DECIDE,n);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(z);CHKERRQ(ierr);
-  ierr = VecDuplicate(z,&y);CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_WORLD,&u_old);CHKERRQ(ierr);
+  ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);
+  ierr = VecSetSizes(u_old,PETSC_DECIDE,n);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(u_old);CHKERRQ(ierr);
+  ierr = VecDuplicate(u_old,&u);CHKERRQ(ierr);
 
-  ierr = VecGetOwnershipRange(z,&rstart,&rend);CHKERRQ(ierr);
-  ierr = VecGetLocalSize(z,&nlocal);CHKERRQ(ierr);
-
+  ierr = VecGetOwnershipRange(u_old,&rstart,&rend);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(u_old,&nlocal);CHKERRQ(ierr);
 
   ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
   ierr = MatSetSizes(A,nlocal,nlocal,n,n);CHKERRQ(ierr);
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
   ierr = MatSetUp(A);CHKERRQ(ierr);
 
-  // ierr = MatCreate(PETSC_COMM_WORLD,&B);CHKERRQ(ierr);
-  // ierr = MatSetSizesBA,PETSC_DECIDE,nlocal,1,n);CHKERRQ(ierr);
-  // ierr = MatSetFromOptions(B);CHKERRQ(ierr);
-  // ierr = MatSetUp(B);CHKERRQ(ierr);
-
+  /*set value for mat A*/
   if (!rstart) 
   {
     rstart = 1;
-    i      = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0; value_vec=1.0;
+    i      = 0; col[0] = 0; col[1] = 1; value[0] = beta; value[1] = alpha; value_vec=alpha;
     ierr   = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
-    ierr   = VecSetValues(z,1,&i,&value_vec,INSERT_VALUES);CHKERRQ(ierr);
+    // ierr   = VecSetValues(z,1,&i,&value_vec,INSERT_VALUES);CHKERRQ(ierr);
   }
   
   if (rend == n) 
   {
     rend = n-1;
-    i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = -1.0; value[1] = 2.0;
+    i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = alpha; value[1] = beta;
     ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
   }
 
   /* Set entries corresponding to the mesh interior */
-  value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
+  value[0] = alpha; value[1] = beta; value[2] = alpha;
   for (i=rstart; i<rend; i++) 
   {
     col[0] = i-1; col[1] = i; col[2] = i+1;value_vec=0.0;
     ierr   = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
-    ierr   = VecSetValues(z,1,&i,&value_vec,INSERT_VALUES);CHKERRQ(ierr);
+    // ierr   = VecSetValues(z,1,&i,&value_vec,INSERT_VALUES);CHKERRQ(ierr);
   }
 
   /* Assemble the matrix and vec */
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = VecAssemblyBegin(z);CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(z);CHKERRQ(ierr);
+
+  /*set value for vec u_old*/
+  if(rank == 0)
+  {
+    value_vec = 0;
+    ierr = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
+    for(i = 1; ii < n; ii++)
+    {
+      xi = i*dx;
+      value_vec = exp(xi) + sin(xi*M_PI)*dt/rho/c;
+	    ierr = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
+    }
+    value_vec = 0;
+    ierr = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
+  }
+
+  ierr = VecAssemblyBegin(u_old);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(u_old);CHKERRQ(ierr);
 
   ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
 
@@ -82,28 +105,18 @@ int main(int argc,char **args)
   ierr = PCSetFromOptions(pc);CHKERRQ(ierr);
 
   
-  for ( i = 0; i < maxit; i++)
+  for ( i = 0; i < steps; i++)
   {
-    ierr = KSPSolve(ksp,z,y);CHKERRQ(ierr);
-    ierr = VecNorm(y,NORM_2,&norm);CHKERRQ(ierr);
-    ierr = VecScale(y,(PetscScalar)1.0/norm);CHKERRQ(ierr);
-    ierr = VecCopy(y,z);CHKERRQ(ierr);
-    if(PetscAbsScalar(norm-norm_old) <= tol){
-      ierr = PetscPrintf(PETSC_COMM_WORLD,"yk is so close to yk-1\n");CHKERRQ(ierr);
-      break;
-    }
-    norm_old = norm; 
+    ierr = KSPSolve(ksp,u_old,u);CHKERRQ(ierr);
+    ierr = VecCopy(u_old,u);CHKERRQ(ierr); 
   }
 
   ierr = PetscPrintf(PETSC_COMM_WORLD,"The eigenvector is :\n");CHKERRQ(ierr);
   ierr = VecView(z, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-  /*caculate eigenvalue */
-   ierr = MatMult(A,z,y);CHKERRQ(ierr);
-   ierr = VecDot(z,y,&lambda);CHKERRQ(ierr);
-   ierr = PetscPrintf(PETSC_COMM_WORLD,"The eigenvector is : %g\n",(double)lambda);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"The eigenvector is : %g\n",(double)lambda);CHKERRQ(ierr);
 
-  ierr = VecDestroy(&z);CHKERRQ(ierr); ierr = VecDestroy(&y);CHKERRQ(ierr);
+  ierr = VecDestroy(&u_old);CHKERRQ(ierr); ierr = VecDestroy(&u);CHKERRQ(ierr);
   ierr = MatDestroy(&A);CHKERRQ(ierr);
   ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
 
