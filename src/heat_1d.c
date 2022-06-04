a6ce47b14ede4a52aff907fb4bb11319f1774d07
lq-debuger <lfszliqiang@163.com>
2022-06-04 22:01:59 +0800

    finish restart

--- src/heat_1d.c
+++ src/heat_1d.c
@@ -9,12 +9,16 @@ int main(int argc,char **args)
 {
   Vec            u_old, u,f;          
   Mat            A;
+  #ifdef IMPLICIT  
   KSP            ksp;
-  PC             pc;                  
+  PC             pc; 
+  #endif                 
   PetscReal      kappa,dt,dx,rho,c,alpha,beta,zero,pi;  
   PetscErrorCode ierr;
-  PetscInt       i,n = 128,col[3],rstart,rend,rank,nlocal,steps,end;
+  PetscInt       i,n = 128,col[3],rstart,rend,rank,nlocal,steps,end,step;
   PetscScalar    value[3],value_vec=0.0,value_f=0.0,xi;
+  PetscViewer    viewer;
+  PetscBool      restart = PETSC_FALSE;
 
   kappa= 1.0;
   dt   = 0.00002;
@@ -23,6 +27,7 @@ int main(int argc,char **args)
   rho  = 1.0;
   c    = 1.0;
   pi   = 4.0*atan(1);
+  step = 1;
   #ifdef IMPLICIT
   alpha= -(kappa*dt/rho/c/dx/dx);
   #elif EXPLICIT
@@ -35,9 +40,7 @@ int main(int argc,char **args)
 
   ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
-
-  ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
-  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-restart",&restart,NULL);CHKERRQ(ierr);
 
   ierr = VecCreate(PETSC_COMM_WORLD,&u_old);CHKERRQ(ierr);
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);
@@ -69,7 +72,6 @@ int main(int argc,char **args)
     ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
   }
 
-  /* Set entries corresponding to the mesh interior */
   value[0] = alpha; value[1] = beta; value[2] = alpha;
   for (i=rstart; i<rend; i++) 
   {
@@ -82,31 +84,55 @@ int main(int argc,char **args)
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatView(A, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-  /*set value for vec u_old and f*/
-  if(rank == 0)
+  /*set value for vec u_old */
+  if(restart)
   {
-    i         = 0;
-    ierr      = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
-    ierr      = VecSetValues(f, 1, &i, &value_f, INSERT_VALUES);CHKERRQ(ierr);
-    for(i = 1; i < n-1; i++)
+    PetscPrintf(PETSC_COMM_WORLD,"Reading u_old from h5 ...\n");
+    #ifdef IMPLICIT
+    ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"implicit.h5",FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+    #endif
+    #ifdef EXPLICIT
+    ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5",FILE_MODE_READ,&viewer);CHKERRQ(ierr);
+    #endif
+    ierr = PetscObjectSetName((PetscObject) u_old, "restart_Vec");CHKERRQ(ierr);
+    ierr = VecLoad(u_old,viewer);CHKERRQ(ierr);
+    ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+  }else
+  {
+    PetscPrintf(PETSC_COMM_WORLD,"Do not read u_old from uold.h5 ...\n");
+    if(rank == 0)
     {
-      xi        = i*dx;
-      value_vec = exp(xi);
-      value_f   = sin(xi*pi)*dt/rho/c;
-	    ierr      = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
-      ierr      = VecSetValues(f, 1, &i, &value_f, INSERT_VALUES);CHKERRQ(ierr);
+      i         = 0;
+      ierr      = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
+      for(i = 1; i < n-1; i++)
+      {
+        xi        = i*dx;
+        value_vec = exp(xi);
+        ierr      = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
+      }
+      value_vec   = 0;
+      ierr        = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
     }
-    value_vec   = 0;
-    value_f     = sin((n-1)*dx*pi)*dt/rho/c;
-    ierr        = VecSetValues(u_old, 1, &i, &value_vec, INSERT_VALUES);CHKERRQ(ierr);
-    ierr        = VecSetValues(f, 1, &i, &value_f, INSERT_VALUES);CHKERRQ(ierr);
   }
-
+  
   ierr = VecAssemblyBegin(u_old);CHKERRQ(ierr);
   ierr = VecAssemblyEnd(u_old);CHKERRQ(ierr);
+
+   /*set value for vec u_old f*/
+  if(rank == 0)
+  {
+    i         = 0; 
+    ierr      = VecSetValues(f, 1, &i, &value_f, INSERT_VALUES);CHKERRQ(ierr);
+    for(i = 1; i < n; i++)
+    {
+      xi        = i*dx;
+      value_f   = sin(xi*pi)*dt/rho/c;
+      ierr      = VecSetValues(f, 1, &i, &value_f, INSERT_VALUES);CHKERRQ(ierr);
+    }
+  }
+
   ierr = VecAssemblyBegin(f);CHKERRQ(ierr);
   ierr = VecAssemblyEnd(f);CHKERRQ(ierr);
-  // ierr = VecView(u_old, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   #ifdef IMPLICIT
   ierr = PetscPrintf(PETSC_COMM_WORLD,"It's implicit scheme\n");CHKERRQ(ierr);
@@ -121,7 +147,8 @@ int main(int argc,char **args)
 
   ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
   ierr = PCSetFromOptions(pc);CHKERRQ(ierr);
-
+  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"implicit.h5",FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) u, "restart_Vec");CHKERRQ(ierr);
   
   for ( i = 0; i < steps; i++)
   {
@@ -133,12 +160,21 @@ int main(int argc,char **args)
       ierr = VecAssemblyBegin(u);CHKERRQ(ierr);
       ierr = VecAssemblyEnd(u);CHKERRQ(ierr);
     }
-    ierr = VecCopy(u,u_old);CHKERRQ(ierr); 
+    ierr = VecCopy(u,u_old);CHKERRQ(ierr);
+    /*write value of u to uold.h5*/ 
+    if(0 == step % 10)
+    {
+      ierr = VecView(u,viewer);CHKERRQ(ierr);
+    }
+    step++;
   }
+  ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   #endif
 
   #ifdef EXPLICIT
   ierr = PetscPrintf(PETSC_COMM_WORLD,"It's explicit scheme\n");CHKERRQ(ierr);
+  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5",FILE_MODE_WRITE,&viewer);;CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) u, "restart_Vec");CHKERRQ(ierr);
   for ( i = 0; i < steps; i++)
   {
     ierr = MatMult(A,u_old,u);CHKERRQ(ierr);
@@ -150,22 +186,26 @@ int main(int argc,char **args)
       ierr = VecAssemblyEnd(u);CHKERRQ(ierr);
     }
     ierr = VecCopy(u,u_old);CHKERRQ(ierr);
+    /*write value of u to uold.h5*/
+    if(0 == step % 10)
+    {
+      ierr = VecView(u,viewer);CHKERRQ(ierr);
+    }
+    step++;
   }
+  ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   #endif
 
   ierr = PetscPrintf(PETSC_COMM_WORLD,"---------------------\n");CHKERRQ(ierr);
   ierr = VecView(u_old, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  // ierr = PetscPrintf(PETSC_COMM_WORLD,"The eigenvector is :\n");CHKERRQ(ierr);
-  // ierr = VecView(z, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-
   ierr = PetscPrintf(PETSC_COMM_WORLD,"The program is finished\n");CHKERRQ(ierr);
 
   ierr = VecDestroy(&u_old);CHKERRQ(ierr); 
   ierr = VecDestroy(&u);CHKERRQ(ierr);
   ierr = VecDestroy(&f);CHKERRQ(ierr);
-
-  #ifdef IMPLICIT
+  // ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
   ierr = MatDestroy(&A);CHKERRQ(ierr);
+  #ifdef IMPLICIT
   ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
   #endif
 
